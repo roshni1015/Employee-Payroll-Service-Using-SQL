2f185c9709992b7cbd4ce0ad198db3faa958b932
roshni1015 <roshniadatrao1110@gmail.com>
2022-06-04 20:06:06 +0530

    [Roshni]-Create Table For Database

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,2 +1,6 @@
+/*UC1 to create a payroll service database*/
 create database Employee_Payroll_service;
 use Employee_Payroll_service
+
+/* UC2 create a employee payroll table in the payroll service database*/
+create table Employee_payroll(Id int identity(1,1) primary key,Name varchar(20),Salary varchar(10),StartDate varchar(20))
