4620796a5b6dd1136e0f7f5b7c63e0ff99315468
roshni1015 <roshniadatrao1110@gmail.com>
2022-06-04 20:08:40 +0530

    [Roshni]-Inserting Data To Table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -4,3 +4,10 @@ use Employee_Payroll_service
 
 /* UC2 create a employee payroll table in the payroll service database*/
 create table Employee_payroll(Id int identity(1,1) primary key,Name varchar(20),Salary varchar(10),StartDate varchar(20))
+
+/* UC3 create employee payroll data */
+insert into employee_payroll(Name, Salary, StartDate) values ('Roshni','40000','26/08/2021')
+insert into employee_payroll(Name, Salary, StartDate) values ('Lavanya','50000','25/08/2021')
+insert into employee_payroll(Name, Salary, StartDate) values ('Mahesh','60000','27/08/2021')
+insert into employee_payroll(Name, Salary, StartDate) values ('Vivek','35000','26/08/2021')
+insert into employee_payroll(Name, Salary, StartDate) values ('Priya','25000','26/08/2021')
