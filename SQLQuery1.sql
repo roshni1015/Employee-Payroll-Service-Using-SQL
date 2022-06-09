37b760b4ad5059fdf75eff7d1d4e9b4e04c9bff4
roshni1015 <roshniadatrao1110@gmail.com>
2022-06-09 16:14:19 +0530

    [Roshni]-Store Employee Information Like Phone, Address, Department

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -45,38 +45,17 @@ Select COUNT(Salary) From Employee_payroll Where Gender='F' Group by Gender
 /* UC8 extend employee_payroll data and save more details */
 ALTER TABLE Employee_payroll ADD Phone varchar(20), Address varchar(100) DEFAULT 'Empty' WITH VALUES, Department varchar(40) NOT NULL DEFAULT 'Developer';
 
-/* UC9 extend employee_payroll table to have Basic Pay, Deductions, Taxable Pay, Income Tax, Net Pay*/
-ALTER TABLE employee_payroll ADD BasicPay Float Not Null default 0; 
-ALTER TABLE employee_payroll ADD Deductions Float Not Null default 0; 
-ALTER TABLE employee_payroll ADD TaxablePay Float Not Null default 0;
-ALTER TABLE employee_payroll ADD IncomeTax Float Not Null default 0;
-ALTER TABLE employee_payroll ADD NetPay Float Not Null default 0;
-
-select * from Employee_payroll
-
-UPDATE Employee_payroll set BasicPay=30000.50 where Name='Roshni';
-UPDATE Employee_payroll set BasicPay=40000 where Name='Lavanya';
-UPDATE Employee_payroll set BasicPay=50000 where Name='Mahesh';
-UPDATE Employee_payroll set BasicPay=25000 where Name='Vivek';
-UPDATE Employee_payroll set BasicPay=15000.75 where Name='Priya';
-
-/* UC10 Insert all values to the table */
-ALTER TABLE Employee_payroll DROP COLUMN Salary
 
  Update Employee_payroll set 
  Phone=9999888877 ,Address='Pune', Department='FrontEnd', Deductions=2000, TaxablePay=1000, IncomeTax=2000, NetPay=25000 where name='Roshni'
  Update Employee_payroll set 
  Phone=9988558899 ,Address='Banglore', Department='Accounts', Deductions=3000, TaxablePay=1000, IncomeTax=3000, NetPay=33000 where name='Lavanya'
  Update Employee_payroll set 
- Phone=8877995544 ,Address='Mumbai', Department='Sales and Marketing', Deductions=2000, TaxablePay=1000, IncomeTax=4000, NetPay=43000 where name='Mahesh'
+ Phone=8877995544 ,Address='Mumbai', Department='Sales', Deductions=2000, TaxablePay=1000, IncomeTax=4000, NetPay=43000 where name='Mahesh'
  Update Employee_payroll set 
- Phone=7744112233 ,Address='Chennai', Department='Testing', Deductions=1500, TaxablePay=1000, IncomeTax=1000, NetPay=21500 where name='Vivek'
+ Phone=7744112233 ,Address='Chennai', Department='Marketing', Deductions=1500, TaxablePay=1000, IncomeTax=1000, NetPay=21500 where name='Vivek'
  Update Employee_payroll set 
  Phone=7775553330 ,Address='Kolkata', Department='Backend', Deductions=1000, TaxablePay=500, IncomeTax=0, NetPay=13500 where name='Priya'
 
 select * from Employee_payroll
 
-Insert into Employee_payroll 
-(Name, Gender, Phone, Address, Department, StartDate, BasicPay, Deductions, TaxablePay, IncomeTax, NetPay) values ('Roshni','F',8887779999,'Pune','Sales and Marketing','28/08/2021',20000,2000,1000,0,17000)
-
-select * from Employee_payroll where Name='Roshni'
